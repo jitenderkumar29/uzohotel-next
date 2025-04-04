e69bb2fb7dea44af1ff7b656145169e2dc2b245e
Jitender Kumar <jitenderkumar2929@gmail.com>
2025-04-04 21:05:30 +0530

    remove unused packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Banner from "@/components/banner/Banner";
+
 import Header from "@/components/Header";
-import Header1 from "@/components/header1/Header1";
-import Header2 from "@/components/header2/Header2";
-import LoginPage from "@/pages/LoginPage";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import Link from "next/link";
 import "./Header.css"; // Import the CSS file
 import Header1 from "./header1/Header1";
 import Header2 from "./header2/Header2";
